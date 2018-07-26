61dfb1779f4fcde7b9b8cf3759aa5f2681262eb3
pmv-nv <pmv-nv@mail.ru>
2018-07-26 15:09:44 +0500

    + Layout авторизация

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "../../node_modules/@angular/core";
+import { RouterModule, Routes } from "../../node_modules/@angular/router";
+import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
+import { SiteLayoutComponent } from "./shared/layouts/site-layout/site-layout.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { RegisterPageComponent } from "./register-page/register-page.component";
+
+const routes: Routes = [
+    {
+        path: '', component: AuthLayoutComponent, children: [
+            {path: '', redirectTo: '/login', pathMatch: 'full'},
+            {path: 'login', component: LoginPageComponent},
+            {path: 'register', component: RegisterPageComponent}
+        ]
+    },
+    {
+        path: '', component: SiteLayoutComponent, children: [
+            
+        ]
+    }    
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
